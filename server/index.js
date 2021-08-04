782dc270f07f0cd44462b516c18154824cec2427
Nikolay Pleshkov <icelol@abv.bg>
2021-08-04 18:24:33 +0300

    update user.js and some node_modules from the client

--- server/index.js
+++ server/index.js
@@ -1,17 +1,43 @@
 const express = require('express');
-const PORT = process.env.PORT | 4000;
-
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
 const mongoose = require('mongoose');
 
+const PORT = process.env.PORT | 4000;
+const app = express();
+//Connecting to the DB
 const _db_conn = require('./config/keys.js').MongoURI;
-
 mongoose.connect(_db_conn, {userNewUrlParser: true}).then(console.log('status connection: 200')).catch(err => console.log(err));
 
 
-const app = express();
+//Bodyparser
+app.use(express.urlencoded( {extended: false }));
 
-app.get('/', (req,res) => {
-    res.send('yes');
-});
+//Express Session
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true,
+}));
+
+//Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Connect flash
+app.use(flash());
+
+//Global var
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+
+    next();
+})
+
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/user'));
 
 app.listen(PORT, console.log(`Server is listening on port ${PORT}`));
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,7 +1,105 @@
 const router = require('express').Router();
+const bcrypt = require('bcrypt');
+const passport = require('passport');
 
-const User = require('../models/user');
-
-router.post('/login', (req, res) => {
-    const { email, password } = req.body;
+//User Model
+const User = require('../models/User');
+//Login Page
+router.get('/login', (req, res) => {
+    res.render('login');
 });
+
+router.post('/login', (req, res, next ) => {
+    passport.authenticate('local',{
+        successRedirect: '/dashboard',
+        failureRedirect: '/users/login',
+        failureFlash: true
+    })(req, res, next);
+});
+
+//Register Page
+router.get('/register', (req, res) => {
+    res.send('register');
+});
+
+//Register Handle
+router.post('/register', (req, res) => {
+    const { name, email, password, confPass } = req.body;
+    let errors = [];
+
+    //Checking required fields
+    if(!name || !email || !password || !confPass){
+        errors.push({msg: 'Please fill in all fields'});
+    }
+ 
+    //Password matching check
+    if(password !== confPass){
+        errors.push({msg: 'Password do not match'});
+    }
+
+    //Check pass length
+    if(password.length < 6){
+        errors.push({msg: 'Password must be at least 6 characters'});
+    }
+
+    if(errors.length > 0){
+        //Register is not successfull
+        res.render('register', {
+            errors,
+            name,
+            email,
+            password,
+            confPass
+        });
+    }
+    else{
+        //Everything is OK
+        User.findOne({ email: req.body.email}).then( user => {
+            if(user){
+                //User exist
+                errors.push({ msg: 'Email is already in use'});
+                res.render('register', {
+                    errors,
+                    name,
+                    email,
+                    password,
+                    confPass
+                });     
+            }
+            else{
+                const regUser = new User({
+                    name,
+                    email,
+                    password
+                });
+
+                //Hash Password
+                bcrypt.genSalt(10, (err, salt) =>
+                 bcrypt.hash(regUser.password, salt, (err, hash) => {
+                     if(err) throw err;
+
+                     regUser.password = hash;
+                     regUser.save()
+                        .then( user => {
+                            req.flash('success_msg', 'Registration successful')
+                            res.redirect('/users/login');
+                        })
+                        .catch(err => console.log(err));
+                }));
+            }
+        });
+
+
+    }
+
+});
+
+
+
+router.get('/logout', (req, res) => {
+    req.logout();
+    req.flash('success_msg', 'Logged out successfuly');
+    res.redirect('/users/login');
+})
+
+module.exports = router;
