872c588d105f58447d5b06aa4d15788d05ef03d4
Nikolay Pleshkov <icelol@abv.bg>
2021-08-06 17:46:23 +0300

    Update GroupTable --almost perfect

--- client/src/components/GroupTable/table.js
+++ client/src/components/GroupTable/table.js
@@ -1,13 +1,173 @@
-import React from 'react'
-import './Table.css'
+import React from "react";
+import "./Table.css";
 function Table() {
-    return (
-        <div className="table-container">
-            <div className="table-header">
-                <h1>table</h1>
+  return (
+    <div className="table-container">
+        
+      <div class="table-wr container">
+        <div class="row table-wr">
+          <div class="col-sm gr-form">
+            <div className="gr-title">Back-End Groups</div>
+            <div className="gr-title-low">Topics</div>
+            <div className="gr-members">
+              Members: <span>2</span>
             </div>
+            <div className="join-btn">
+              <button className="join-btn-stl">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-person-plus-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                  <path
+                    fill-rule="evenodd"
+                    d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+
+          <div class="col-sm gr-form">
+            <div className="gr-title">Back-End Groups</div>
+            <div className="gr-title-low">Topics</div>
+            <div className="gr-members">
+              Members: <span>2</span>
+            </div>
+            <div className="join-btn">
+              <button className="join-btn-stl">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-person-plus-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                  <path
+                    fill-rule="evenodd"
+                    d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+
+
+          <div class="col-sm gr-form">
+            <div className="gr-title">Back-End Groups</div>
+            <div className="gr-title-low">Topics</div>
+            <div className="gr-members">
+              Members: <span>2</span>
+            </div>
+            <div className="join-btn">
+              <button className="join-btn-stl">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-person-plus-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                  <path
+                    fill-rule="evenodd"
+                    d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+
+
+          <div class="col-sm gr-form">
+            <div className="gr-title">Back-End Groups</div>
+            <div className="gr-title-low">Topics</div>
+            <div className="gr-members">
+              Members: <span>2</span>
+            </div>
+            <div className="join-btn">
+              <button className="join-btn-stl">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-person-plus-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                  <path
+                    fill-rule="evenodd"
+                    d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+
+
+          <div class="col-sm gr-form">
+            <div className="gr-title">Back-End Groups</div>
+            <div className="gr-title-low">Topics</div>
+            <div className="gr-members">
+              Members: <span>2</span>
+            </div>
+            <div className="join-btn">
+              <button className="join-btn-stl">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-person-plus-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                  <path
+                    fill-rule="evenodd"
+                    d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
+
+          <div class="col-sm gr-form gr-br-n">
+            <div className="gr-title">Back-End Groups</div>
+            <div className="gr-title-low">Topics</div>
+            <div className="gr-members">
+              Members: <span>2</span>
+            </div>
+            <div className="join-btn">
+              <button className="join-btn-stl">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-person-plus-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                  <path
+                    fill-rule="evenodd"
+                    d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default Table
+export default Table;
