e6a2052c79cbe478a3b48c0feac9ad46f77e4e86
Nikolay Pleshkov <icelol@abv.bg>
2021-08-09 10:56:56 +0300

    fix overflow --final fixgit add client/src/components!

--- client/src/components/Navbar/navbar.js
+++ client/src/components/Navbar/navbar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 import { Button, ButtonLogin } from "../Buttons/priamry-btn";
 import { openLogin,closeLogin, openRegister } from "../../actions";
